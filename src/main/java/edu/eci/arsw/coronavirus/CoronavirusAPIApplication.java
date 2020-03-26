312c7409c2d23417eeba0a88e65bf6ef545b93a6
Juan Villate <juan.villate-i@mail.escuelaing.edu.co>
2020-03-26 18:52:59 +0530

    cricleci and heroku

---
+++ src/main/java/edu/eci/arsw/coronavirus/CoronavirusAPIApplication.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.coronavirus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author jmvillatei
+ */
+@SpringBootApplication
+public class CoronavirusAPIApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(CoronavirusAPIApplication.class, args);
+    }
+
+}
+
